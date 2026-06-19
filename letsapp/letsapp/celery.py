d51e1bbc12a205fe492bfc2da5295429106c4e57
simeonsare <simeonsare254@gmail.com>
2026-06-20 02:10:41 +0300

    introduced leaflet for pinning dropoof and pickup pointd

--- letsapp/letsapp/celery.py
+++
@@ -1,9 +0,0 @@
-# letsapp/letsapp/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letsapp.settings')
-
-app = Celery('letsapp')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
